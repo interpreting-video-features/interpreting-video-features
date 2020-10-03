a81af41cdf2ce227da09df1aca720d80b6511350
Sofia Broomé <sbroome@kth.se>
2020-10-03 15:19:13 +0200

    cleanup

--- video_features_pytorch/models/CLSTM_4.py
+++ video_features_pytorch/models/CLSTM_4.py
@@ -1,88 +1,91 @@
-'''
-based on implementation from https://github.com/automan000/Convolution_LSTM_pytorch
-'''
-
+"""
+Based on implementation from https://github.com/automan000/Convolution_LSTM_pytorch
+"""
 import torch
-import torch.nn as nn
-import torch.nn.functional as F
-from torch.autograd import Variable
-
-import numpy as np
-
-import os
-import sys
-from collections import OrderedDict
 from models.convolution_lstm import ConvLSTM
 
-class Model(nn.Module):
-    
 
-    def __init__(self, num_classes=174, nb_lstm_units=32, channels=3, conv_kernel_size=(5,5), pool_kernel_size=(2,2), top_layer=True, avg_pool=False, batch_normalization=True, lstm_layers=4, step=16, image_size=(224,224), dropout=0, conv_stride=(1,1), effective_step=[4, 8, 12, 15], useEntireSeq=False, addSoftMax=False):
-        
+class Model(torch.nn.Module):
+    def __init__(self, num_classes=174, nb_lstm_units=32, channels=3, conv_kernel_size=(5, 5), pool_kernel_size=(2, 2),
+                 top_layer=True, avg_pool=False, batch_normalization=True, lstm_layers=4, step=16,
+                 image_size=(224, 224), dropout=0, conv_stride=(1, 1), effective_step=[4, 8, 12, 15],
+                 useEntireSeq=False, addSoftMax=False):
+
         super(Model, self).__init__()
 
-        self.num_classes=num_classes
-        self.nb_lstm_units=nb_lstm_units
-        self.channels=channels
-        self.top_layer=top_layer
-        self.avg_pool=avg_pool
-        self.c_kernel_size=conv_kernel_size
+        self.num_classes = num_classes
+        self.nb_lstm_units = nb_lstm_units
+        self.channels = channels
+        self.top_layer = top_layer
+        self.avg_pool = avg_pool
+        self.c_kernel_size = conv_kernel_size
         self.lstm_layers = lstm_layers
         self.step = step
-        self.im_size=image_size
+        self.im_size = image_size
         self.pool_kernel_size = pool_kernel_size
         self.batch_normalization = batch_normalization
-        self.dropout=dropout
+        self.dropout = dropout
         self.conv_stride = conv_stride
-        self.effective_step=effective_step
-        self.addSoftMax=addSoftMax
+        self.effective_step = effective_step
+        self.addSoftMax = addSoftMax
         self.UseEntireSeq = useEntireSeq
         self.build()
-        
-        
-    
+
+        self.firstCLSTMLayer = None
+        self.endFC = None
+        self.sm = None
+
     def build(self):
-        '''
+        """
         pytorch CLSTM usage: 
-        clstm = ConvLSTM(input_channels=512, hidden_channels=[128, 64, 64], kernel_size=5, step=9, effective_step=[2, 4, 8])
+        clstm = ConvLSTM(input_channels=512, hidden_channels=[128, 64, 64],
+                         kernel_size=5, step=9, effective_step=[2, 4, 8])
         lstm_outputs = clstm(cnn_features)
         hidden_states = lstm_outputs[0]
 
-        '''
-        #self.firstConvLayer = nn.Conv2d(self.channels, self.nb_lstm_units, kernel_size=self.c_kernel_size, stride=1, dilation=(1, 1), padding=(1, 1))
-        self.firstCLSTMLayer = ConvLSTM(input_channels=self.channels, \
-                                        hidden_channels=[self.nb_lstm_units]*self.lstm_layers,\
-                                       kernel_size=self.c_kernel_size[0], conv_stride = self.conv_stride, pool_kernel_size=self.pool_kernel_size, step=self.step, effective_step=self.effective_step, batch_normalization=self.batch_normalization,dropout=self.dropout)
+        """
+        self.firstCLSTMLayer = ConvLSTM(input_channels=self.channels,
+                                        hidden_channels=[self.nb_lstm_units] * self.lstm_layers,
+                                        kernel_size=self.c_kernel_size[0], conv_stride=self.conv_stride,
+                                        pool_kernel_size=self.pool_kernel_size, step=self.step,
+                                        effective_step=self.effective_step,
+                                        batch_normalization=self.batch_normalization, dropout=self.dropout)
 
-        
-        if(self.UseEntireSeq):
-            self.endFC = nn.Linear(in_features=len(self.effective_step)*self.nb_lstm_units*int(self.im_size[0]/((self.conv_stride*self.pool_kernel_size[0])**self.lstm_layers))*int(self.im_size[1]/((self.conv_stride*self.pool_kernel_size[0])**self.lstm_layers)), out_features=self.num_classes)
+        if self.UseEntireSeq:
+            self.endFC = torch.nn.Linear(in_features=len(self.effective_step) * self.nb_lstm_units * int(
+                self.im_size[0] / ((self.conv_stride * self.pool_kernel_size[0]) ** self.lstm_layers)) * int(
+                self.im_size[1] / ((self.conv_stride * self.pool_kernel_size[0]) ** self.lstm_layers)),
+                                         out_features=self.num_classes)
         else:
-            self.endFC = nn.Linear(in_features=self.nb_lstm_units*int(self.im_size[0]/((self.conv_stride*self.pool_kernel_size[0])**self.lstm_layers))*int(self.im_size[1]/((self.conv_stride*self.pool_kernel_size[0])**self.lstm_layers)), out_features=self.num_classes)
-        
+            self.endFC = torch.nn.Linear(in_features=self.nb_lstm_units * int(
+                self.im_size[0] / ((self.conv_stride * self.pool_kernel_size[0]) ** self.lstm_layers)) * int(
+                self.im_size[1] / ((self.conv_stride * self.pool_kernel_size[0]) ** self.lstm_layers)),
+                                         out_features=self.num_classes)
+
         print("use entire sequence is: ", self.UseEntireSeq)
         print("shape of FC is: ", self.endFC)
         self.sm = torch.nn.Softmax(dim=1)
-        
-        
+
     def forward(self, x):
 
         output, hiddens = self.firstCLSTMLayer(x)
 
-        
-        if(self.UseEntireSeq):
-            output = self.endFC(torch.stack(output).view(-1,len(self.effective_step)*self.nb_lstm_units*int(self.im_size[0]/((self.conv_stride*self.pool_kernel_size[0])**self.lstm_layers))*int(self.im_size[1]/((self.conv_stride*self.pool_kernel_size[0])**self.lstm_layers))))
+        if self.UseEntireSeq:
+            output = self.endFC(torch.stack(output).view(-1, len(self.effective_step) * self.nb_lstm_units * int(
+                self.im_size[0] / ((self.conv_stride * self.pool_kernel_size[0]) ** self.lstm_layers)) * int(
+                self.im_size[1] / ((self.conv_stride * self.pool_kernel_size[0]) ** self.lstm_layers))))
         else:
-            output = self.endFC(output[-1].view(-1,self.nb_lstm_units*int(self.im_size[0]/((self.conv_stride*self.pool_kernel_size[0])**self.lstm_layers))*int(self.im_size[1]/((self.conv_stride*self.pool_kernel_size[0])**self.lstm_layers))))
-        
-        if(self.addSoftMax):
+            output = self.endFC(output[-1].view(-1, self.nb_lstm_units * int(
+                self.im_size[0] / ((self.conv_stride * self.pool_kernel_size[0]) ** self.lstm_layers)) * int(
+                self.im_size[1] / ((self.conv_stride * self.pool_kernel_size[0]) ** self.lstm_layers))))
+
+        if self.addSoftMax:
             output = self.sm(output)
-            
-        return output   
+
+        return output
 
     def extract_features(self, x):
         for end_point in self.VALID_ENDPOINTS:
             if end_point in self.end_points:
                 x = self._modules[end_point](x)
         return self.avg_pool(x)
-
